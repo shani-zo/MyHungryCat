9235e9c4374ca7ecba8c71ac180b26750844f2d0
Shani <shani93zohar@gmail.com>
2021-04-10 17:07:42 +0300

    =Add a function to classify images using google cloud vision sdk as service provider

--- imageClassification.py
+++ imageClassification.py
@@ -8,15 +8,30 @@ from google.cloud import vision
 AUTHENTICATION_KEY_PATH = r"C:\Users\shani\Downloads\dyinterview-8a7ec547ca93.json"
 
 
-def authenticate():
+def authenticate() -> None:
     """Try setting up credentials for using google Cloud vision API"""
     if not os.path.exists(AUTHENTICATION_KEY_PATH):
         print("Path to authentication key was not found. If no other existing key is found an error will be shown.")
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = AUTHENTICATION_KEY_PATH
 
 
-authenticate()
+def classify_image(image_content: bytes):
+    """Send image to classification engine and return resulted labels"""
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=image_content)
 
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)
+
+
+authenticate()
+"""
 # Google example code
 
 # Instantiates a client
@@ -38,3 +53,4 @@ labels = response.label_annotations
 print('Labels:')
 for label in labels:
     print(label.description)
+"""
