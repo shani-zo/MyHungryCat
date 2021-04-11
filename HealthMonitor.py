88b2293df5372cdb8082b219b46aa8ede686f058
Shani <shani93zohar@gmail.com>
2021-04-11 20:24:50 +0300

    Implemented the mailing functions for the health monitor so that alerts can be sent to user

--- HealthMonitor.py
+++ HealthMonitor.py
@@ -1,8 +1,8 @@
 import time
 
-from tools import run_periodically
 import feeding_data
-
+from tools import run_periodically
+from mailing import MailingService
 
 FEEDING_INTERVAL = 15
 HEALTH_CHECK_INTERVAL = 15
@@ -14,6 +14,8 @@ class HealthMonitor:
         self.warning_sent = False
         self.feeding_interval = feeding_interval
         self.health_check_interval = health_check_interval
+        self.mailing_service = MailingService()
+        self.alerts_email_subscriber = 'dy@gmail.com'
 
     @property
     def last_feeding_time(self) -> int:  # TODO
@@ -41,16 +43,18 @@ class HealthMonitor:
         new_food_timestamp = 0
         self.update_last_feeding_time(new_food_timestamp)
 
-    def send_warning(self):  # SHOULD CHANGE TO EVENT? TODO
-        pass
+    def send_warning(self):
+        self.mailing_service.send_new_message(self.alerts_email_subscriber, 'ALERT! The cat is hungry!',
+                                              'The cat han\'t been fed recently. Please feed fast!!!')
 
-    def send_back_to_normal(self):  # TODO
-        pass
+    def send_back_to_normal_email(self):
+        self.mailing_service.send_new_message(self.alerts_email_subscriber, 'Back To Normal',
+                                              'The cat has been fed again! Thanks!')
 
     def check_feeding_state(self):
         if self.cat_has_been_fed:
             if self.warning_sent:
-                self.send_back_to_normal()
+                self.send_back_to_normal_email()
         else:
             if not self.warning_sent:
                 self.send_warning()
@@ -60,4 +64,3 @@ if __name__ == '__main__':
     health_checker = HealthMonitor(FEEDING_INTERVAL, HEALTH_CHECK_INTERVAL)
     run_periodically(health_checker.check_for_new_food(), 1)
     run_periodically(health_checker.check_feeding_state(), HEALTH_CHECK_INTERVAL)
-
