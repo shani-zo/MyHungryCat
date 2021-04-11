5a067e2874efb8248257ac8583d4f38292c76e6f
Shani <shani93zohar@gmail.com>
2021-04-11 19:19:20 +0300

    Add a function that checks if a file is an image

--- tools.py
+++ tools.py
@@ -1,8 +1,9 @@
 import time
 import sched
+import imghdr
 
 
-def run_periodically(func, itter_time):
+def run_periodically(func: callable, itter_time: int):
     """Create scheduler for running a function periodically"""
     scheduler = sched.scheduler(time.time, time.sleep)
 
@@ -11,3 +12,22 @@ def run_periodically(func, itter_time):
         scheduler.enter(itter_time, 1, run, (scheduler,))
     scheduler.enter(itter_time, 1, run, (scheduler,))
     scheduler.run()
+
+
+def is_image_file(file_path) -> bool:
+    """
+    Will return if the input file is an image file.
+
+    Args:
+        file_path: path to check
+
+    Returns:
+        Is the path given belongs to an image file
+
+    Raises:
+        FileNotFoundError if the image file doesn't exist.
+
+    """
+    return bool(imghdr.what(file_path))
+
+
